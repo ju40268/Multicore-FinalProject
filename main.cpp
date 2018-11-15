c9e4af83375fa15c4b61f0e02aa6fd32fa88413f
may811204 <ju40268@gmail.com>
2018-11-14 18:43:39 -0600

    connected component finalized

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 void kruskal() {
-    Graph* g = new Graph("test");
+    Graph* g = new Graph("test3");
     g->print();
     vector<Edge>* graph = g->graph;
     int total_nodes = g->total_nodes;
@@ -40,15 +40,84 @@ void kruskal() {
             d->merge(s1, s2);
         }
     }
-    cout << "final cost : " << mstcost << endl;
+    cout << "final kruskal cost : " << mstcost << endl;
     cout << "mst : " << endl;
     mst->print();
 }
 
 void prim() {
+    Graph* g = new Graph("test3");
+    priority_queue<Edge, vector<Edge>, EdgeCompare> pq;
+    vector<Edge>* graph = g->graph;
+    int total_nodes = g->total_nodes;
+    bool visited[total_nodes];
 
+    Graph* mst = new Graph(total_nodes);
+
+    int start = 0, count = 0, mstcost = 0, cur = 0;
+    // mark everything node as not visited
+    for (int i = 0; i < total_nodes; i++) {
+        visited[i] = false;
+    }
+    for(int i = 0; i < graph[start].size(); i++) {
+        pq.push(graph[start].at(i));
+    }
+    visited[start] = true;
+    while (count < total_nodes && !pq.empty()) {
+        Edge e = pq.top(); pq.pop();
+        if (!visited[e.source] || !visited[e.destination]) {
+            mst->add(e);
+            mstcost += e.cost;
+            if (!visited[e.source]) {
+                visited[e.source] = true;
+                cur = e.source;
+            }
+            if (!visited[e.destination]) {
+                visited[e.destination] = true;
+                cur = e.destination;
+            }
+            for (Edge neighbour : graph[cur]) {
+                pq.push(neighbour);
+            }
+            count++;
+        }
+    }
+    cout << "final prim cost : " << mstcost << endl;
+    cout << "mst : " << endl;
+    mst->print();
+} 
+
+
+void dfs(int v, bool visited[], vector<Edge>* graph) {
+    visited[v] = true;
+    cout << v << " "; 
+    for (Edge e : graph[v]) {
+        if (!visited[e.destination]) dfs(e.destination, visited, graph);
+    }
+}
+
+void connected_component() {
+    Graph* g = new Graph("test4");
+    vector<Edge>* graph = g->graph;
+    int total_nodes = g->total_nodes;
+    bool *visited = new bool[total_nodes]; 
+    int component_count = 0;
+
+    for(int i = 0; i < total_nodes; i++) 
+        visited[i] = false; 
+  
+    for (int i = 0; i < total_nodes; i++) {
+        if (!visited[i]) {
+            dfs(i, visited, graph);
+            component_count++;
+            cout << endl;
+        }
+    }
+    cout << "Total component count : " << component_count << endl;
 }
 
 int main(int argc, char** argv) {
-    kruskal();
+    connected_component();
+    // kruskal();
+    // prim();
 }
