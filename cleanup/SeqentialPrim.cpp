ea24d23248182323bc6f28bf8f185b3aa4f14c67
may811204 <ju40268@gmail.com>
2018-11-17 16:32:44 -0600

    clean

--- cleanup/SeqentialPrim.cpp
+++ cleanup/SeqentialPrim.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <climits>
 #include <chrono>
+#include <thread>
+#include "PriorityQueue.h"
+
 #define INF INT_MAX
 using namespace std;
 using namespace chrono;
@@ -10,85 +13,94 @@ using namespace chrono;
 typedef pair<int, int> iPair;
 ifstream input ("test.txt");
 
-priority_queue< iPair, vector <iPair> , greater<iPair> > pq;
-vector<int> key;
-vector<int> parent;
-vector<bool> inMST;
+void sequentialAdd(int v, int u, int weight);
+FineGrainedHeap pq;
 
 class Graph {
     int V;   
     public:
+        vector<int> key;
+        vector<int> parent;
+        vector<bool> inMST;
         list< pair<int, int> > *adj;
-        Graph(int V); 
+        Graph(); 
+        void init(int V);
         void addEdge(int u, int v, int w);
-        void primMST();
+        void primMST(int V);
 };
 
-Graph::Graph(int V) {
+Graph::Graph() {}
+void Graph::init(int V) {
     this->V = V;
-    adj = new list<iPair> [V];
+    adj = new list<pair<int, int> > [V];;
 }
 void Graph::addEdge(int u, int v, int w) {
     adj[u].push_back(make_pair(v, w));
     adj[v].push_back(make_pair(u, w));
 }
-void mst(int v, int u, int weight) {
-    if (inMST[v] == false && key[v] > weight) {
-        // Updating key of v
-        key[v] = weight;
-        pq.push(make_pair(key[v], v));
-        parent[v] = u;
-    }
-}
-void Graph::primMST() {   
-    pq.push(make_pair(0, 0));
+
+void Graph::primMST(int V) {   
+    pq.insert(0, 0, 0);
     key[0] = 0;
-    while (!pq.empty()) {
-        int u = pq.top().second;
-        pq.pop();
- 
+
+    int pqsize = V;
+    while (pqsize != 0) {
+        int u = pq.removeMin().second;
         inMST[u] = true; 
+
         list< pair<int, int> >::iterator i;
         for (i = adj[u].begin(); i != adj[u].end(); ++i) {
             int v = (*i).first;
             int weight = (*i).second;
-            mst(v,u,weight);
+            sequentialAdd(v, u, weight);
         }
+        pqsize--;
     }
-    printf("MST:\nV1 - V2\n");
-    for (int i = 1; i < V; ++i)
-        printf("%d - %d\n", parent[i], i);
+
+    printf(" --- MST Result --- :\nSource - Destination\n");
+        for (int i = 1; i < V; ++i)
+            printf("%d - %d\n", parent[i], i);
+
 }
- 
-int main() {
-
-    int V,E;
-    input>>V;
-    input>>E;
-    Graph g(V);
-
-    key.resize(V,INF);
-    parent.resize(V,-1);
-    inMST.resize(V,false);
-    int source, destination, weight;
-    for(int i = 0; i < E; i++) {
-        input >> source >> destination >>weight;
-        g.addEdge(source,destination,weight);
+Graph g;
+void sequentialAdd(int v, int u, int weight) {
+    if (g.inMST[v] == false && g.key[v] > weight) {
+        g.key[v] = weight;
+        pq.insert(v, ID, weight);
+        g.parent[v] = u;
     }
-    cout << "\n --- Graph Description ---\n\tNo. Of vertices : "<<V<<"\n\tNo. Of edges : "<<E<<"\n";
+}
+int main() {
+    int V, E;
+    input >> V;
+    input >> E;
+    g.init(V);
+    g.key.resize(V, INF);
+    g.parent.resize(V, -1);
+    g.inMST.resize(V, false);
+    pq.init(V);
+    int source, destination, weight;
+
+    for (int i = 0; i < E; i++) {
+        input >> source >> destination >> weight;
+        g.addEdge(source, destination, weight);
+    }
+
+    cout << "\n --- Graph Description --- \n\tNo. Of vertices : " << V << "\n\tNo. Of edges : " << E << "\n";
     for (int u = 0; u < V; ++u) {
         cout << "\t" << u << ": ";
-        for(auto i = g.adj[u].begin(); i != g.adj[u].end(); ++i) {
+        for (auto i = g.adj[u].begin(); i != g.adj[u].end(); ++i) {
             int v = (*i).first;
             int w = (*i).second;
             cout << "(" << v << "," << w << ") --> ";
         }
-        cout<< "END" << endl;
+        cout << "END" << endl;
     }
-  
+
     auto start = std::chrono::high_resolution_clock::now();
-    g.primMST();
+    g.primMST(V);
     auto totaltime = std::chrono::high_resolution_clock::now() - start;
-    printf("Total time consumed: %lld \n", totaltime);
+    printf("Execution time for parallel Prim's is %lld microseconds\n", totaltime);
+
     return 0;
 }
